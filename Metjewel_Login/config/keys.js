04e73192cde131c91596cc825029828512519ff8
Nadzirah Zahiruddin <nadziratul.zahiruddin@hotmail.com>
2021-06-15 23:08:12 +0800

    Add files via upload
    upload 2 folders

---
+++ Metjewel_Login/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://metjewel_admin:hello123456@cluster0.fxb7x.mongodb.net/test_mongo'
+    //mongoURI: "mongodb+srv://MainUser:H@yd3n248@cluster0.yqrn1.mongodb.net/testmetjewel?retryWrites=true&w=majority"
+}
